314cbf1ad67dfebdc741c8f18d62358802acb1ec
Abunasyr <abunasyr1@gmail.com>
2024-03-23 22:42:46 +0500

    feat: add EventBus.ts in app

--- src/core/EventBus.ts
+++ src/core/EventBus.ts
@@ -1,3 +1,32 @@
-class EventBus {
+type EventListeners = (...args: any[]) => void
 
+class EventBus {
+    private listeners: {[event: string]: EventListeners[] } = {};
+
+    on (event: string, callback: EventListeners): void {
+        if (!this.listeners[event]) {
+            this.listeners[event] = []
+        }
+
+        this.listeners[event].push(callback)
+    }
+
+    off (event: string, callback: EventListeners): void {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события ${event}`)
+        }
+
+        this.listeners[event] = this.listeners[event].filter(
+            (listener) => listener !== callback)
+    }
+
+    emit(event: string, ...args: any[]) {
+        if (this.listeners[event]) {
+            throw new Error(`Нет события ${event}`)
+        }
+
+        this.listeners[event].forEach((listener) => listener(...args))
+    }
 }
+
+export default EventBus
